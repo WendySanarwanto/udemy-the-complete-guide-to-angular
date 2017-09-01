7f13bf0ef129e57db6437874c0a09ac7fa3bdeda
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-09-02 06:44:36 +0800

    Set the component's selector by attribute name or CSS style class.

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',
+  // tslint:disable-next-line:component-selector
+  // selector: '[app-servers]',
+  // tslint:disable-next-line:component-selector
+  selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
