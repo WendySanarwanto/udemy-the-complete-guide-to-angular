3c5c5f65ce07b1fa7ef6dd2e4b40853bc4fe8643
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-09-04 06:35:25 +0800

    Added ngStyle on the server component's <p> tag.

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -9,6 +9,14 @@ export class ServerComponent {
     serverId = 10;
     private serverStatus = 'offline';
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
+
     getServerStatus() {
         return this.serverStatus;
     }
