4b55c2065c3759b14d9ce33abe5594bed3810d84
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-09-12 05:17:51 +0800

    Cleaned code.

--- assignments/basics-assignment3-start/src/app/logs-panel/logs-panel.component.ts
+++ assignments/basics-assignment3-start/src/app/logs-panel/logs-panel.component.ts
@@ -1,31 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logs-panel',
   templateUrl: './logs-panel.component.html',
   styleUrls: ['./logs-panel.component.css']
 })
-export class LogsPanelComponent implements OnInit {
+export class LogsPanelComponent {
   showContent = false;
   BUTTON_DISPLAY_ACTION = 'Display';
   BUTTON_HIDE_ACTION = 'Hide';
   buttonActionLabel = this.BUTTON_DISPLAY_ACTION;
   buttonClickLogs: string[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
   onButtonClicked() {
     // Toggle the displaying of that paragraph with the button created in the first step
     this.showContent = !this.showContent;
     this.buttonActionLabel = this.showContent ? this.BUTTON_HIDE_ACTION : this.BUTTON_DISPLAY_ACTION;
-    
+
     this.logButtonClick();
   }
 
-  private logButtonClick(){
+  private logButtonClick() {
     const timestamp = Date();
     const logMessage = `Display button is clicked at ${timestamp}`;
     this.buttonClickLogs.push(logMessage);
