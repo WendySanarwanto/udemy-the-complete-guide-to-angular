b39062f695fed653f39a77ba424c066a7239f80f
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-09-18 21:29:45 +0800

    Declare a list of Recipe in the RecipeListComponent

--- course-projects/section3-the-basics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-projects/section3-the-basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { Recipe } from '../recipe.model';
+import { IRecipe } from '../recipe.contract';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: IRecipe[] = [
+    {
+      name: `'Nasi Kuning' recipe`,
+      description: `This is the recipe of delicious Indonesian's Yellow Rice.`,
+      imagePath: `http://bit.ly/2wpsyfx`
+    }
+    // new Recipe(`'Nasi Kuning' recipe`,
+    //            `This is the recipe of delicious Indonesian's Yellow Rice.`,
+    //            `http://bit.ly/2wpsyfx`)
+  ];
 
   constructor() { }
 
