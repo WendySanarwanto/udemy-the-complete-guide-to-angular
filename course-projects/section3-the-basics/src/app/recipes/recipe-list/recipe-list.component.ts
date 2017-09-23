2a043e5cb16ea9f9e2d961fed5e460948d8a7f3c
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-09-23 12:35:24 +0800

    Added more recipe data items

--- course-projects/section3-the-basics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-projects/section3-the-basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,21 +8,26 @@ import { IRecipe } from '../recipe.contract';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: IRecipe[] = [
-    {
+  recipes: IRecipe[] = [ {
       name: `'Nasi Kuning' recipe`,
       description: `This is the recipe of delicious Indonesian's Yellow Rice.`,
       imagePath: `http://bit.ly/2wpsyfx`,
-    },
-    {
+    }, {
       name: `'Nasi Rawon' recipe`,
       description: `This is the recipe of tasty Indonesian's 'Nasi Rawon' (Beef Black Coloured Soup)`,
       imagePath: `http://bit.ly/2xl5ihF`,
-    },
-    {
+    }, {
       name: `'Tahu Campur' recipe`,
       description: `Recipe of 'Tahu Campur' (Diced Tofu with Vegetables, beef meat & broth)`,
       imagePath: `http://bit.ly/2xgiylO`,
+    }, {
+      name: `'Nasi Liwet' recipe`,
+      description: `Recipe of 'Nasi Liwet', tasteful chicken rice mixed with choconut cream.`,
+      imagePath: `http://bit.ly/2jPRGWG`,
+    }, {
+      name: `'Soto Ayam Lamongan' recipe`,
+      description: `Recipe of 'Soto Ayam Lamongan', Chicken Rice in Yellow broth.`,
+      imagePath: `http://bit.ly/2wc2vEw`,
     }
     // new Recipe(`'Nasi Kuning' recipe`,
     //            `This is the recipe of delicious Indonesian's Yellow Rice.`,
