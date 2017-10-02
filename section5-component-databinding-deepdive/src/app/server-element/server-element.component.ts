3aa2e02c7da95344f0950607eef7dd6278c90de8
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-10-03 06:42:45 +0800

    Added comments about ViewEncapsulation

--- section5-component-databinding-deepdive/src/app/server-element/server-element.component.ts
+++ section5-component-databinding-deepdive/src/app/server-element/server-element.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { IServer } from '../../shared/server.contract';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None,
+   // None -> No encapsulation, styles apply globally; Native -> same as emlulated but only work on modern browsers.
+   // Emulated -> It mimicks Native for sake of supporting old browsers and it's the default ViewEncapsulation
 })
+
 export class ServerElementComponent implements OnInit {
   @Input() server: IServer;
 
