905873639708c7646b77651eb88f4a0eecfd1a9c
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-10-07 21:56:28 +0800

    Added component lifecycle examples.

--- section5-component-databinding-deepdive/src/app/server-element/server-element.component.ts
+++ section5-component-databinding-deepdive/src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
 import { IServer } from '../../shared/server.contract';
 
 @Component({
@@ -10,12 +23,44 @@ import { IServer } from '../../shared/server.contract';
    // Emulated -> It mimicks Native for sake of supporting old browsers and it's the default ViewEncapsulation
 })
 
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, 
+  OnChanges, OnDestroy, OnInit, DoCheck {
   @Input() server: IServer;
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log(`[DEBUG] - 'ServerElement': constructor is called.`);
   }
 
+  ngOnInit() {
+    console.log(`[DEBUG] - 'ServerElement': 'ngOnInit' is called.`);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(`[DEBUG] - 'ServerElement': 'ngOnChanges' is called. 'changes'=`, changes);
+  }
+
+  ngDoCheck() {
+    console.log(`[DEBUG] - 'ServerElement': 'ngDoCheck' is called.`);
+  }
+
+  ngAfterContentInit() {
+    console.log(`[DEBUG] - 'ServerElement': 'ngAfterContentInit' is called.`);
+  }
+
+  ngAfterContentChecked() {
+    console.log(`[DEBUG] - 'ServerElement': 'ngAfterContentChecked' is called.`);
+  }
+
+  ngAfterViewInit() {
+    console.log(`[DEBUG] - 'ServerElement': 'ngAfterViewInit' is called.`);
+  }
+
+  ngAfterViewChecked() {
+    console.log(`[DEBUG] - 'ServerElement': 'ngAfterViewChecked' is called.`);
+  }
+
+  ngOnDestroy() {
+    console.log(`[DEBUG] - 'ServerElement': 'ngOnDestroy' is called.`);
+  }
 }
